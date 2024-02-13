a9df047b4f007f931c3112d6d1c001343f36e3aa
mahdieh m.salehi <msalehi.mahdieh@gmail.com>
2024-02-13 22:40:12 +0330

    cookies

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,13 +6,14 @@ const overlay = document.querySelector('.overlay');
 const modal = document.querySelector(".popup__openaccount");
 const modalCloseBtn = document.querySelector(".popup__openaccount-closebtn");
 const openModal = document.querySelectorAll(".open__modal");
+const header = document.querySelector(".header");
 
 // --------------------------------functions-------------------------
 
 const toggleVisibilty = function (e) {
     modal.classList.toggle("hidden");
     overlay.classList.toggle('hidden');
-    console.log(e);
+    //console.log(e);
     e.preventDefault();
 }
 // ----------------------------------btns--------------------------------
@@ -24,3 +25,15 @@ overlay.addEventListener("click", toggleVisibilty);
 addEventListener("keydown", function (e) {
     if (e.key == 'Escape') toggleVisibilty();
 });
+
+const message = document.createElement('div');
+message.classList.add('cookies');
+message.innerHTML = 'we used cookied for improved functionality and analytics.<button class="menu__btn active btn__close-cookies">Got it</button>';
+header.prepend(message);
+// header.append(message.cloneNode(true));
+document.querySelector(".btn__close-cookies").addEventListener("click", function () {
+    // header.remove(message) that wrong
+    // message.remove();
+    // header.removeChild(message);
+    message.parentElement.removeChild(message);
+})
